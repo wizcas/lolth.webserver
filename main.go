1b73fee24d9427cb45d780d6164b5a289d373883
0x1C <chen@wizcas.me>
2019-06-03 15:30:15 +0800

    使用中间件解析path，将资源文件重定位到BaseURL

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strings"
 
 	"quasar-ai.com/bast/marketing.landing/logger"
 
@@ -13,19 +14,29 @@ import (
 	"quasar-ai.com/bast/marketing.landing/caching"
 )
 
-var PORT = helpers.EnvVar{Key: "PORT", DefaultValue: "8080"}.GetInt()
-var INDEX_PATH = helpers.EnvVar{Key: "INDEX_PATH", DefaultValue: "index.html"}.GetString()
+// Port 服务器监听端口
+var Port = helpers.EnvVar{Key: "PORT", DefaultValue: "8080"}.GetInt()
+
+// IndexPath 首页文件相对路径(相对于BaseUrl)
+var IndexPath = helpers.EnvVar{Key: "INDEX_PATH", DefaultValue: "index.html"}.GetString()
+
+var assetSuffixes = []string{
+	".js", ".png", ".jpg", ".jpeg", ".gif", ".svg", ".ico",
+	".txt",
+	".webmanifest",
+}
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	r.Use(subpathMiddleware(r))
 	r.GET("/", index)
-	logger.WithTag("STARTUP").WriteInfo("server is listening on %d", PORT)
-	r.Run(fmt.Sprintf(":%d", PORT))
+	logger.WithTag("STARTUP").WriteInfo("server is listening on %d", Port)
+	r.Run(fmt.Sprintf(":%d", Port))
 }
 
 func index(c *gin.Context) {
-	if data, err := caching.GetCachedContent(INDEX_PATH); err != nil {
+	if data, err := caching.GetCachedContent(IndexPath); err != nil {
 		var status int
 		switch err.(type) {
 		case caching.ContentFetchError:
@@ -39,4 +50,32 @@ func index(c *gin.Context) {
 	} else {
 		c.Data(200, "text/html", data)
 	}
+}
+
+func subpathMiddleware(r *gin.Engine) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		relpath := c.Request.URL.Path
+		if redirectToAsset(relpath, c) {
+			return
+		}
+		if relpath == "/" {
+			logger.WithTag("REQ").WriteDebug("ROOT")
+			c.Next()
+		} else {
+			logger.WithTag("REQ").WriteDebug("REDIRECT TO ROOT")
+			c.Request.URL.Path = "/"
+			r.HandleContext(c)
+		}
+	}
+}
+
+func redirectToAsset(relpath string, c *gin.Context) bool {
+	logger.WithTag("REQ").WriteDebug("URL: %s", relpath)
+	for _, suffix := range assetSuffixes {
+		if strings.HasSuffix(relpath, suffix) {
+			c.Redirect(http.StatusMovedPermanently, helpers.ResolveURL(relpath))
+			return true
+		}
+	}
+	return false
 }
